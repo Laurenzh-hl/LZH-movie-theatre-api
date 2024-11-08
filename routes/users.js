de428555e9d23f9ac0e1c01ed990dacfbed36146
Lauren Zell-Huxley <lauren.zell-huxley@hl.co.uk>
2024-11-08 10:22:30 +0000

    user router, app, and server created

---
+++ routes/users.js
@@ -0,0 +1,43 @@
+const { Router } = require("express");
+const { User, Show } = require("../models/index");
+const router = Router();
+const { check, validationResult, matchedData } = require("express-validator");
+
+router.get("/", async function(req, res) {
+    const users = await User.findAll();
+    res.json(users);
+});
+
+router.get("/:userId", async function(req, res) {
+    const user = await User.findByPk(req.params.userId);
+
+    if (!user) {
+        res.status(404).json({ error: "User not found" });
+        return;
+    }
+
+    res.json(user);
+});
+
+router.get("/:userId/shows", async function (req, res) {
+    const user = await User.findByPk(req.params.userId);
+
+    if(!user) {
+        res.status(200).json([]);
+        return;
+    }
+
+    const shows = user.getShows();
+    res.json(shows);
+});
+
+router.put("/:userID/shows/:showId", async function (req, res) {
+    const user = await User.findByPk(req.params.userId);
+    const show = await Show.findByPk(req.params.showId);
+
+    await user.addShow(show)
+    res.send("Movie has been added to user's watched list");
+});
+
+module.exports = router;
+
